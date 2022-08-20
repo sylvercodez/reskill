6e674a64f8d6105aaeba401f2a0c9696118c6936
otenaike oluwatiobi <64742528+sylvercodez@users.noreply.github.com>
2022-08-20 11:26:22 +0100

    Update quasar.conf.js

--- quasar.conf.js
+++ quasar.conf.js
@@ -19,7 +19,7 @@ module.exports = configure(function (ctx) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/boot-files
-    boot: ['axios'
+    boot: [
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
@@ -230,4 +230,4 @@ module.exports = configure(function (ctx) {
       },
     }
   }
-});
+}});
